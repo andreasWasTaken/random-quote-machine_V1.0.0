0101d1ab9da8df4972ed987476178be574f2f194
Andreas <a_eder@gmx.net>
2020-08-08 11:05:08 +0200

    Version 1.1.0 - added option for german quotes

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const $quoteText = document.getElementById('quoteText')
 const $quoteAuthor = document.getElementById('quoteAuthor')
 const $nextQuoteBtn = document.getElementById('nextQuoteBtn')
 
+const $langDE = document.getElementById('langDE')
+const $langEN = document.getElementById('langEN')
+
+let language = 'langEN'
+let source = './data/quotesEN.json'
+
+
 async function getQuote(index) {
-    const rawQuotes = await fetch('quotes.json')
+    const rawQuotes = await fetch(source)
     const quotes = await rawQuotes.json()
     
     $quoteText.textContent = quotes[index].text
@@ -13,13 +20,15 @@ async function getQuote(index) {
 }
 
 async function getRandomQuote() {
-    const rawQuotes = await fetch('quotes.json')
+    
+    const rawQuotes = await fetch(source)
     const quotes = await rawQuotes.json()
 
     const random = Math.floor(Math.random() * quotes.length)
     
     $quoteText.innerHTML = quotes[random].text
     $quoteAuthor.textContent = quotes[random].author ? `- ${quotes[random].author}` : 'No Author'
+    
 }
 
 function chooseRandomColor(colors) {
@@ -33,11 +42,25 @@ const colors = randomColor({
 })
 console.log(colors);
 
-root.style.setProperty('--dominant-clr', chooseRandomColor(colors))
+root.style.setProperty('--clr-random', chooseRandomColor(colors))
 getRandomQuote()
 
 $nextQuoteBtn.addEventListener('click', () => {
-    root.style.setProperty('--dominant-clr', chooseRandomColor(colors))
+    root.style.setProperty('--clr-random', chooseRandomColor(colors))
+    getRandomQuote()
+})
+
+$langDE.addEventListener('click', (event) => {
+    language = event.target.id
+    source = './data/quotesDE.json'
+    $nextQuoteBtn.textContent = 'Neues Zitat'
+    getRandomQuote()
+})
+
+$langEN.addEventListener('click', (event) => {
+    language = event.target.id
+    source = './data/quotesEN.json'
+    $nextQuoteBtn.textContent = 'New quote'
     getRandomQuote()
 })
 
